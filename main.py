a0432feb5ee3d0c9916e2b0eeaaf4ceaa39360fe
Bala Tom <balatom14@gmail.com>
2026-09-07 22:55:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -27,8 +27,6 @@ def create_default_hostel_blocks():
             "B205": {"capacity": 4, "students": []}
         },
 
-        
-
         "Block C": {
             "C301": {"capacity": 3, "students": []},
             "C302": {"capacity": 3, "students": []},
